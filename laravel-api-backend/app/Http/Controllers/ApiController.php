5c2963e03ef0ec342f60f818bacb826e04da9221
sirio95 <sirio.iasevoli@gmail.com>
2023-02-21 17:21:07 +0100

    added routesto edit/update/delete movies

--- laravel-api-backend/app/Http/Controllers/ApiController.php
+++ laravel-api-backend/app/Http/Controllers/ApiController.php
@@ -27,4 +27,73 @@ class ApiController extends Controller
             ]
         ]);
     }
+    public function movie_store(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required|string|min:3',
+            'director' => 'required|string|min:0',
+            'prod_year' => 'required|integer|min:0',
+            'expenses' => 'required|integer|min:0',
+            'cashout' => 'required|integer|min:0',
+            'genre_id' => 'required|integer|min:1',
+            'tags_id' => 'nullable|array',
+        ]);
+
+        $genre = Genre::find($data['genre_id']);
+        $movie = Movie::make($data);
+
+        $movie->genre()->associate($genre);
+        $movie->save();
+
+        if (array_key_exists('tags_id', $data)) {
+            $tags = Tag::find($data['tags_id']);
+            $movie->tags()->sync($tags);
+        }
+
+        return response()->json([
+            'success' => true,
+            'response' => $movie,
+            'data' => $request->all()
+        ]);
+    }
+
+    public function movie_update(Request $request, Movie $movie)
+    {
+        $data = $request->validate([
+            'name' => 'required|string|min:3',
+            'director' => 'required|string|min:0',
+            'prod_year' => 'required|integer|min:0',
+            'expenses' => 'required|integer|min:0',
+            'cashout' => 'required|integer|min:0',
+            'genre_id' => 'required|integer|min:1',
+            'tags_id' => 'nullable|array',
+        ]);
+
+        $genre = Genre::find($data['genre_id']);
+
+        $movie->update($data);
+        $movie->genre()->associate($genre);
+        $movie->save();
+
+        if (array_key_exists('tags_id', $data)) {
+            $tag = Tag::find($data['tags_id']);
+            $movie->tags()->sync($tag);
+        }
+
+        return response()->json([
+            'success' => true,
+            'response' => $movie,
+            'data' => $request->all()
+        ]);
+    }
+
+    public function movie_delete(Movie $movie)
+    {
+        $movie->tags()->sync([]);
+        $movie->delete();
+
+        return response()->json([
+            'success' => true
+        ]);
+    }
 }
--- laravel-api-backend/routes/api.php
+++ laravel-api-backend/routes/api.php
@@ -6,3 +6,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ApiController;
 
 Route::get('/v1/movie/all', [ApiController::class, 'get_all_movie_tag_genre']);
+
+Route::post('/v1/movie/store', [ApiController::class, 'movie_store']);
+
+Route::post('/v1/movie/update/{movie}', [ApiController::class, 'movie_update']);
+
+Route::delete('v1/movie/delete/{movie}', [ApiController::class, 'movie_delete']);
